c5c1cc17e4d86df48923c975cc30dce0e7a3dabc
Yashith <yashith98@gmial.com>
2024-03-13 13:38:23 +0530

    updated config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function ()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "javascript","java","python"},
-      highlist = { enable = true },
-      indent = { enable = true },
-    })    
-    end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+      auto_install = true,
+			ensure_installed = { "lua", "javascript", "java", "python" },
+			highlist = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
