e3d13c4102a41b3e36d16c615dc2b7a9a5e13d09
yashith <yashith98@gmail.com>
2025-03-31 15:14:02 +0530

    yadadada

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "jdtls", "pyre" },
+				ensure_installed = { "lua_ls", "jdtls" },
 			})
 		end,
 	},
@@ -19,7 +19,7 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.jdtls.setup({})
-			lspconfig.pyre.setup({})
+			--lspconfig.pyre.setup({})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,11 +4,11 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.diognostics.eslint,
+				--null_ls.builtins.formatting.stylua,
+				--null_ls.builtins.formatting.black,
+				--null_ls.builtins.formatting.prettier,
+				--null_ls.builtins.formatting.isort,
+        -- null_ls.builtins.diognostics.eslint
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
