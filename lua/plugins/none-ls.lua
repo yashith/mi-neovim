b58605158e78abc802d385d7a0e1755bb605ffb9
yashith <yashith98@gmail.com>
2025-04-04 23:04:34 +0530

    that line

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,7 +4,7 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				--null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.stylua,
 				--null_ls.builtins.formatting.black,
 				--null_ls.builtins.formatting.prettier,
 				--null_ls.builtins.formatting.isort,
